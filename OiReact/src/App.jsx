767919ea8940817577324dfec116e016da43fc1d
Emi-Souza <emilly_l_souza@estudante.sesisenai.org.br>
2024-08-23 17:16:14 -0300

    Criação do primeiro projeto com React bonitão

---
+++ OiReact/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Dinossauros</h1>
+     <h2>Carnívoros</h2>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis at nihil commodi veniam consectetur, corrupti <br />
+      fugit eum rem, dolor, explicabo fugiat dolores quam fuga sapiente ipsum animi dignissimos vero. Exercitationem.</p>
+    </>
+  )
+}
+
+export default App
