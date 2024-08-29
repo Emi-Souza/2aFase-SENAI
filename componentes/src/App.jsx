860930027f9d5036b848217873bf5f168fafc9d0
Emi-Souza <emilly_l_souza@estudante.sesisenai.org.br>
2024-08-29 17:24:57 -0300

    add componentes

---
+++ componentes/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos de VS code</h1>
+      <h2>Hitória do filme</h2>
+
+      <p>comando para emmet da ul>li*6</p>
+      <ul>
+        <li>Atalho: alt + z para quebra altomática de linha.</li>
+        <li>Atalho: alt + clique esquerdo para adicionar cursores.</li>
+        <li>Atalho:</li>
+        <li>Atalho: ctrl + shift + k para escluir uma linha inteira.</li>
+        <li>Atalho: ctrl + ; para comentar/descomentar.</li>
+        <li>Atalho: windows + . abre teclado de emoji.</li>
+        <li>Atalho: alt + ↕  para mover uma linha para cima ou para baixo.</li>
+        <li>Atalho: alt + shift + ↕ copia a linha atual ou bloco selecionado.</li>
+        <li>Atalho: ctrl + j ou ctrl + ' abre/fecha o terminal</li>
+      </ul>
+      <p>Para esfregar vários cursores pela tela (digite váras coisas ao mesmo tempo): Clicar com a rodinha e esfregar pela tela.</p>
+
+      <p>Extenções:</p>
+      <ul>
+        <li>ES7 React/Redux/GraphQL/React-Native snippets</li>
+        <li>Auto Rename Tag</li>
+        <li>Color Highlight</li>
+        <li>Power Mode</li>
+        <li>vscode-pets</li> 
+        <li>GlassIt-VSC</li>
+      </ul>
+      
+    </>
+  )
+}
+
+export default App
