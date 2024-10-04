efd74c3e563df96562455b5c7fdcb8e54856ad2f
emi-souza <emilly_l_souza@estudante.sesisenai.org.br>
2024-10-04 17:29:18 -0300

    Template

--- farmarcia/src/Components/Farmacia.jsx
+++ farmarcia/src/Components/Farmacia.jsx
@@ -1,42 +1,32 @@
-import React from 'react';
+import React from 'react'
 import './Farmacia.css'
 
 function Farmacia() {
   return (
-<<<<<<< HEAD
     <>
-    <div className="teste-container">
+      <div className="teste-container">
 
-      <div className='textos'>
-        <h1>Bem-vindo à Nossa Nova Casa!</h1>
-        <h2>Venha Celebrar Conosco!</h2>
-        <p>
-          Estamos animados em anunciar a inauguração do nosso novo negócio! <br />
-          Prepare-se para um dia repleto de alegria, surpresas e, claro, muitas novidades!
-        </p>
+        <div className='textos'>
+
+          <h1>Bem-vindo à Nossa Nova Casa!</h1>
+
+          <h2>Venha Celebrar Conosco!</h2>
+
+          <p>
+            Estamos animados em anunciar a inauguração do nosso novo negócio! <br />
+            Prepare-se para um dia repleto de alegria, surpresas e, claro, muitas novidades!
+          </p>
+
+        </div>
+
+        <div className='corpo'>
+          <img src="./images/cartoes.png" className='img-cartoes' />
+          <img src="./images/camisetas.png" className='img-camisetas' />
+        </div>
       </div>
-
-      <div className='corpo'>
-        <img src="./images/cartoes.png" className='img-cartoes' />
-        <img src="./images/camisetas.png" className='img-camisetas' />
-      </div>
-    </div>
-
     </>
-  );
-=======
-    <div className='texto'>
-      <h1>🎉 Bem-vindo à Nossa Nova Casa! 🎉</h1>
 
-
-      <h2>Venha Celebrar Conosco!</h2>
-
-      <p>Estamos animados em anunciar a inauguração do nosso novo negócio! <br />
-        Prepare-se para um dia repleto de alegria, surpresas e, claro, muitas novidades!</p>
-
-    </div>
   )
->>>>>>> ce2168c0bb1a005bd11d864da554df4188d34fa3
 }
 
-export default Farmacia;
+export default Farmacia
--- farmarcia/src/Components/Navbar.jsx
+++ farmarcia/src/Components/Navbar.jsx
@@ -4,30 +4,21 @@ import Logo from './Logo'
 
 function Navbar() {
   return (
-<<<<<<< HEAD
     <div className='navbar-container'>
       <div className='logo'>
-      <Logo/>
+        <Logo />
       </div>
-=======
-    <div>
-      <div  className='navbar-container'>
-        <img className='img' src="./Images/farmarcia.png" alt="" />
->>>>>>> ce2168c0bb1a005bd11d864da554df4188d34fa3
-        <ul class="nav-links">
-            <button className='button'>Home</button>
-            <button className='button'>Sobre</button>
-            <button className='button'>Serviços</button>
-            <button className='button'>Contato</button>
-        </ul>
-<<<<<<< HEAD
-        
-=======
-      </div>
->>>>>>> ce2168c0bb1a005bd11d864da554df4188d34fa3
+      <img className='img' src="./images/farmacia.png" alt="" />
+      <ul className="nav-links">
+        <button className='button'>Home</button>
+        <button className='button'>Sobre</button>
+        <button className='button'>Serviços</button>
+        <button className='button'>Contato</button>
+      </ul>
     </div>
   )
 }
 
 export default Navbar
+
 
